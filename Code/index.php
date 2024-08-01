3b31d791ec565ba5b5ffc2bba5574bad5c155325
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2024-08-01 17:49:14 +0800

    Add some code.

--- Code/index.php
+++ Code/index.php
@@ -2,7 +2,19 @@
     include_once("header.php");
 ?>
         <main>
-            
+            <div class="index_container">
+                <div class="index_text">
+                    <h1>Discover and Find Your Own Fashion!</h1> 
+                    <p>Explore our curated collection of stylish clothing and accessories tailored to your unique taste.</p>
+                    <a href="shop.php" class="explore_button">Explore Now</a>
+                </div>
+                <div class="index_image">
+                    <img src="purple_star.jpg">
+                </div>
+            </div>
         </main>
+        <?php 
+            include_once("footer.html");
+        ?>
     </body>
 </html>
