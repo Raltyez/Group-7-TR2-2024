d824293de2ac6e355cb20b618ba38fef3b53500e
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2024-08-04 15:18:06 +0800

    Update index.php

--- Code/index.php
+++ Code/index.php
@@ -9,12 +9,30 @@
                     <a href="shop.php" class="explore_button">Explore Now</a>
                 </div>
                 <div class="index_image">
-                    <img src="purple_star.jpg">
+                    <button class="prev" onclick="changeImage(-1)">&#10094;</button>
+                    <img src="https://i.ibb.co/Rvd0kHd/1.png" id="displayedImage">
+                    <button class="next" onclick="changeImage(1)">&#10095;</button>
                 </div>
             </div>
         </main>
-        <?php 
-            include_once("footer.html");
-        ?>
     </body>
 </html>
+
+<?php 
+    include_once("footer.html");
+?>
+
+<script>
+    var images = ["https://i.ibb.co/Rvd0kHd/1.png", "https://i.ibb.co/nk4mN6L/2.png", "https://i.ibb.co/X4qzxGB/3.png"]; // Add image URLs here
+    var currentIndex = 0;
+
+    function changeImage(direction) {
+        currentIndex += direction;
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        } else if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        document.getElementById("displayedImage").src = images[currentIndex];
+    }
+</script>
