21aa715bbd8322c8815a0c163757e20c05fbbee1
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2024-08-07 17:43:49 +0800

    Update contact.php

--- Code/contact.php
+++ Code/contact.php
@@ -4,9 +4,9 @@
         <main>
             <div class="contact_container">
                 <h3>Contact Information</h3>
-                <p><i class="fas fa-phone"></i>+65 1234 5678</p>
-                <p><i class="fas fa-envelope"></i>comitop@gmail.com</p>
-                <p><i class="fas fa-map-marker-alt"></i>149 Sims Dr, Singapore 387380</p>
+                <p><a href="tel:+6512345678"><i class="fas fa-phone"></i>+65 1234 5678</a></p>
+                <p><a href="mailto:vallerianwilson@gmail.com"><i class="fas fa-envelope"></i>comitop@gmail.com</a></p>
+                <p><a href="https://www.google.com/maps/place/James+Cook+University,+Singapore/@1.3158779,103.8732324,17z/data=!3m2!4b1!5s0x31da1831ba6344a5:0x3692adf8ca59f42e!4m6!3m5!1s0x31da1831b3334157:0xef2b9a5526ac77ad!8m2!3d1.3158725!4d103.8758073!16zL20vMGJteXFu?entry=ttu" target="_blank"><i class="fas fa-map-marker-alt"></i>149 Sims Dr, Singapore 387380</a></p>
             </div>
         </main>
     </body>
