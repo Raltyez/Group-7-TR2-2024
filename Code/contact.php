1a244b1754b1feaf91d6cb2428adcb214b7b4b39
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2024-08-07 16:54:39 +0800

    Add content to this page

--- Code/contact.php
+++ Code/contact.php
@@ -2,10 +2,21 @@
     include_once("header.php");
 ?>
         <main>
-            
+            <div class="contact_container">
+                <h3>Contact Information</h3>
+                <p><i class="fas fa-phone"></i>+65 1234 5678</p>
+                <p><i class="fas fa-envelope"></i>comitop@gmail.com</p>
+                <p><i class="fas fa-map-marker-alt"></i>149 Sims Dr, Singapore 387380</p>
+            </div>
         </main>
     </body>
 </html>
 <?php 
     include_once("footer.html");
 ?>
+
+<style>
+main {
+    color: white;
+}
+</style>
