130711e3258e371bd45b00b264f16b59b1e5ecd9
Wilson Vallerian <143167334+Wilson-Vallerian@users.noreply.github.com>
2024-08-10 14:47:06 +0800

    Update reset_password_form.php

--- code/reset_password_form.php
+++ code/reset_password_form.php
@@ -1,19 +1,15 @@
 <?php 
     include_once("header.php");
 ?>
-        <main style="display: flex; justify-content: center; align-items: center; background-image: url('purple_star.jpg'); height: 100vh; margin: 0;">
+        <main style="display: flex; justify-content: center; align-items: center; background-image: url('shirt_background.webp'); height: 100vh; margin: 0;">
             <div class="wrapper">
-                <form action="reset_password.php" method="POST">
-                    <h1>Reset Password</h1>
+                <form action="send_confirmation_code.php" method="POST">
+                    <h1>Forgot Password</h1>
                     <div class="input_box">
-                        <input type="password" placeholder="New Password" required name="new_password">
-                        <i class='bx bx-lock-alt'></i>
+                        <input type="email" placeholder="Enter your email" required name="email">
+                        <i class='bx bx-envelope'></i>
                     </div>
-                    <div class="input_box">
-                        <input type="password" placeholder="Confirm New Password" required name="confirm_new_password">
-                        <i class='bx bx-lock-alt'></i>
-                    </div>
-                    <button type="submit" class="btn">Reset Password</button>
+                    <button type="submit" class="btn">Send Confirmation Code</button>
                 </form>
             </div>
         </main>
